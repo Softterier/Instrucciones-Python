aabcdb0e2c5ffda077d168c8be148d461a4d2832
Softterier <43970002+Softterier@users.noreply.github.com>
2023-12-29 14:15:52 -0500

    Create Clases y Objetos 3.py
    Clases y Objetos

---
+++ Clases y Objetos 3.py
@@ -0,0 +1,74 @@
+Class y Objects.py  SOY DALTO   Youtube
+
+<__main__.celular object at 0x000001CB77F8D290>
+PS C:/Users/Desktop/Curso de POO>
+
+~~    self automatico del objeto -  Referencia a si mismo        ~~
+~~ marca propiedad de self, modelo propiedad de self, camaraF propiedad de self ~~    
+~~ esta Funcion de ejecuta automatiamente cuando ya esta creada o se esta creando ~~
+~~ Funcion constructor, que es la que mira si esta definida la clase ~~
+    
+class estudiante:
+    def __init__(Self,nombre,edad,grado):
+    Self.nombre = nombre
+    Self.edad = edad
+    Self.grado = grado
+
+    def estudiar(self)
+      print("estudiando...") 
+---
+~~ Estudiante = estudiante("Petter", "23", "4 Semestre") 
+~~ Estudiante = estudiante(nombre,edad,grado) 
+
+~~ nombre = input("Digame su nombre: ")
+~~ edad = input("que edad tiene?: ")
+~~ grado = input("Que grado cursa?: ")
+
+~~>El estudiante se llama
+~~>con una edad de
+~~>y estas cursando el
+~~ El estudiante Petter esta estudiando en 4 Semestre.
+
+~~ def estudiar(): 
+~~ print("El estudiante (nombre) esta estudiando (grado).")
+
+---
+print(f"El estudiante se llama "{estudiante.nombre})
+print(f"con una edad de "{estudiante.edad})
+print(f"y estas cursando el "{estudiante.grado})
+
+
+  estudiar =  input()
+  if (estudiar.lower == "estudiar")
+  estudiante.estudiar()
+
+  
+print("""
+  DATOS DEL ESTUDIANTE: \n\n
+  NOMBRE: {celular1.nombre} \n
+  EDAD:  {celular1.edad} \n
+  GRADO:  {celular1.grado} \n
+""")
+while true:
+    estudiar = input()
+    if (estudiar.lower() == "estudiar"):
+        estudiante.estudiar()
+
+~~ def estudiar(): 
+~~ print("El estudiante (nombre) esta estudiando (grado).")
+
+
+> DATOS DEL ESTUDIANTE:
+
+
+  NOMBRE: Petter
+
+  EDAD:  23
+  
+  GRADO:  4 Semestre
+
+estudiar
+Estudiando...
+
+
+
