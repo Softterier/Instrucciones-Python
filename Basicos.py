e640ae8612701055be2f974ae8bdf39bca55839c
Softterier <43970002+Softterier@users.noreply.github.com>
2023-12-29 09:52:24 -0500

    Create Basicos.py
    Conceptos Basicos 1

---
+++ Basicos.py
@@ -0,0 +1,15 @@
+celular_1_marca = "Samsung"
+celular_2_marca = "Apple"
+celular_3_marca = "Huawei"
+
+celular_1_modelo = "S23"
+celular_2_modelo = "I Phone 15 Pro"
+celular_3_modelo = "P20 Pro"
+
+celular_1_camaraT = "48 MP"
+celular_2_camaraT = "48 MP"
+celular_3_camaraT = "12 MP"
+
+celular_1_camaraF = "24 MP"
+celular_2_camaraF = "24 MP"
+celular_3_camaraF = "12 MP"
