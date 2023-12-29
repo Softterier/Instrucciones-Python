9b8ba23efa6601e2d0e6fa3af7af6b205c0e119d
Softterier <43970002+Softterier@users.noreply.github.com>
2023-12-29 10:38:56 -0500

    Create Clases y Objetos.py
    Clases y Objetos 1

---
+++ Clases y Objetos.py
@@ -0,0 +1,18 @@
+~~ snake case se utiliza para el nombre de las variables al principio de cada palabra, el gión bajo:  separado_por_guines_bajos;  nombre_primero y nombre_primer_preferido
+~~ camel case se utilica para el nombre de las variables al principio de cada palabra, mayuscula: primerLetraEnMayusculaElRestoEnMinuscula
+~~ Pascal case se utilica para el nombre de las variables al principio de cada palabra, mayuscula: TodasLasPrimerasLetraEnMayusculaElRestoEnMinuscula
+
+~~ Clase Celular Recomendacio para notificar las Clases y Objetos
+
+class Celular():[
+  marca = "Samsung"
+  modelo = "S23"
+  camaraF = "48MP" 
+
+~~ atributos estaticos
+celular1 = Celular()
+print(celular1.marca)
+
+~~ Instancia dentro de una Clase (multiples) 
+
+
