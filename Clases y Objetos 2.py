91b9c7a250fb52c15b74b797e1c74afccd3447a9
Softterier <43970002+Softterier@users.noreply.github.com>
2023-12-29 11:33:57 -0500

    Create Clases y Objetos 2.py
    Clases y Objetos 2

---
+++ Clases y Objetos 2.py
@@ -0,0 +1,14 @@
+Class y Objects.py
+
+<__main__.celular object at 0x000001CB77F8D290>
+PS C:/Users/Desktop/Curso de POO>
+
+class Celular:
+    def __init__(Self, marca, Modleo, camara):
+    Self.marca
+    Self.modelo
+    Self.camara
+       
+def __init__
+
+
